85ea257fc2ace2de5d46e640ccef1361f31eeca5
dmitryl <dmitrijs.litvinenko@neotech.lv>
2020-02-16 17:49:33 +0200

    adding: default test runner

---
+++ src/test/java/org/craigslist/runners/AbstractTestRunner.java
@@ -0,0 +1,21 @@
+package org.craigslist.runners;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+
+@Slf4j
+public class AbstractTestRunner {
+
+    AbstractTestRunner() {}
+
+    @BeforeClass
+    public static void setUp() {
+        log.info("Set up");
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        log.info("Tear down");
+    }
+}
---
+++ src/test/java/org/craigslist/runners/DefaultTestRunner.java
@@ -0,0 +1,17 @@
+package org.craigslist.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"pretty", "html:target/cucumber", "json:target/cucumber-report.json"},
+        strict = true,
+        stepNotifications = true,
+        features = "classpath:features",
+        glue = "steps.definitions",
+        tags = "@all"
+)
+public class DefaultTestRunner extends AbstractTestRunner {
+}

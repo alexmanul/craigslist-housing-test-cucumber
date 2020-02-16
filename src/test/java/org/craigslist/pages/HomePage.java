ebe1bc3002b947d93bf1c494be14ad01c0bb64e7
dmitryl <dmitrijs.litvinenko@neotech.lv>
2020-02-16 20:34:05 +0200

    adding: logger to home page

--- src/test/java/org/craigslist/pages/HomePage.java
+++ src/test/java/org/craigslist/pages/HomePage.java
@@ -2,16 +2,19 @@ package org.craigslist.pages;
 
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
+import lombok.extern.java.Log;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.$;
 import static org.craigslist.configuration.PropertiesConfiguration.getHomeBaseUrl;
 
+@Log
 public class HomePage {
     private final SelenideElement CRAIGSLIST_LOGO = $("#logo");
     private final SelenideElement HOUSING_TITLE = $("#hhh");
 
     public void open() {
+        log.info("Opening home page: " + getHomeBaseUrl());
         Selenide.open(getHomeBaseUrl());
         CRAIGSLIST_LOGO.shouldBe(visible);
     }
